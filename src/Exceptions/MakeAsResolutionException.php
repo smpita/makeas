b15998865098390433fc00b01ea251932528033d
Sean Pearce <sean@complyant.co>
2023-08-16 23:47:58 -0700

    Add makeAs macro

---
+++ src/Exceptions/MakeAsResolutionException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Smpita\MakeAs\Exceptions;
+
+use Exception;
+
+class MakeAsResolutionException extends Exception
+{
+}
--- src/MakeAsServiceProvider.php
+++ src/MakeAsServiceProvider.php
@@ -2,11 +2,42 @@
 
 namespace Smpita\MakeAs;
 
+use Illuminate\Container\Container;
+use Smpita\MakeAs\Exceptions\MakeAsResolutionException;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
 class MakeAsServiceProvider extends PackageServiceProvider
 {
+    /**
+     * Bootstrap MakeAs service.
+     */
+    public function boot(): void
+    {
+        $this->app->macro('makeAs', function (string $abstract, array $parameters = [], string $expected = null) {
+            /**
+             * Guarantee a resolved object is a given class.
+             *
+             * @template TAbstract of object
+             * @template TExpected of object
+             *
+             * @param  string  $abstract
+             * @param  array  $parameters
+             * @param  class-string<TExpected>|null  $expected
+             * @return ($abstract is class-string<TAbstract> ? ($expected is null ? TAbstract : TExpected) : TExpected)
+             *
+             * @throws MakeAsResolutionException
+             */
+            $expected ??= $abstract;
+
+            if (is_a($concrete = Container::getInstance()->make($abstract, $parameters), $expected)) {
+                return $concrete;
+            }
+
+            throw new MakeAsResolutionException("Target [$expected] is not instantiable.");
+        });
+    }
+
     public function configurePackage(Package $package): void
     {
         /*
